f0d6650718c700dfde436c54968d4a4d6206f04d
pjehan <pierre.jehan@gmail.com>
2025-06-02 15:23:39 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Python
+
+## Installation
+
+```bash
+npm install -g revealexpress
+git clone https://github.com/pjehan-teaching/python
+cd python
+revealexpress
+```
---
+++ slideshow.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Python',
+  stylesheets: [],
+  javascripts: ['assets/js/script.js'],
+  revealjs: {
+    width: '100%',
+    height: '100%'
+  }
+};
